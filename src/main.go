dda665a0a98c3cd3e168d177e0e6a24f6a431055
ryanyen2 <ryanyen2-c@my.cityu.edu.hk>
2021-04-30 09:35:58 +0800

    change tls

--- src/main.go
+++ src/main.go
@@ -67,8 +67,8 @@ func main() {
 
 	// start the server on localhost port 8000 and log any errors
 	log.Println("http server started on :8080")
-	// err = http.ListenAndServeTLS(":8080", "/etc/letsencrypt/live/ryanyen2.me/fullchain.pem", "/etc/letsencrypt/live/ryanyen2.me/privkey.pem", nil)
-	err = http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServeTLS(":8080", "/etc/letsencrypt/live/ryanyen2.me/fullchain.pem", "/etc/letsencrypt/live/ryanyen2.me/privkey.pem", nil)
+	// err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
